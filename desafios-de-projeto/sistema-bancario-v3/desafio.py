962ea9434b618b012f245fcf85a6000376853356
Gabriel <gabrielms.castro@gmail.com>
2024-06-01 15:52:04 -0300

    implementação de menu funcional

--- desafios-de-projeto/sistema-bancario-v3/desafio.py
+++ desafios-de-projeto/sistema-bancario-v3/desafio.py
@@ -1,5 +1,5 @@
 import textwrap
-from abc import ABC, abstractmethod, classmethod
+from abc import ABC, abstractmethod
 from datetime import datetime
 
 class Cliente:
@@ -174,15 +174,15 @@ class Deposito(Transacao):
 def menu():
     menu = """
     ========= MENU =========
-    [d] Depositar
-    [s] Sacar
-    [e] Extrato
-    [nc] Novo cliente
-    [lc] Listar clientes
-    [nu] Novo usuário
-    [q] Sair
-    """
-    print(menu)
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova Conta
+    [lc]\tListar Contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    ==> """
+    return input(textwrap.dedent(menu))
     
 def filtrar_cliente(cpf, clientes):
     clientes_filtrados = [cliente for cliente in clientes if cliente.cpf == cpf]
@@ -199,7 +199,8 @@ def depositar(clientes):
     cpf = input("Informe o CPF:\t")
     cliente = filtrar_cliente(cpf, clientes)
     
-    if not cliente: print("\n@@@ Cliente não localizado. @@@")
+    if not cliente:
+        print("\n@@@ Cliente não localizado. @@@")
     
     valor = float(input("Informe o valor do depósito:\t"))
     transacao = Deposito(valor)
@@ -211,8 +212,83 @@ def depositar(clientes):
     cliente.realizar_transacao(conta, transacao)
     
 def sacar(clientes):
-    pass
+    cpf = input("Informe o CPF:\t")
+    cliente = filtrar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print("\n@@@ Cliente não localizado. @@@")
+    
+    valor = float(input("Informe o valor do saque:\t"))
+    transacao = Saque(valor)
+    
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+    
+    cliente.realizar_transacao(conta, transacao)
 
+def exibir_extrato(clientes):
+    cpf = input("Informe o CPF:\t")
+    cliente = filtrar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print("\n@@@ Cliente não localizado. @@@")
+        return
+    
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+    
+    print("\n========== EXTRATO ==========")
+    transacoes = conta.historico.transacoes
+    
+    extrato = ""
+    if not transacoes:
+        extrato = "Não foram realizadas movimentações."
+    else:
+        for transacao in transacoes:
+            extrato += f"\n{transacao['tipo']}: R$ {transacao['valor']:.2f}"
+    
+    print(extrato)
+    print(f"\nSaldo:\t\tR$ {conta.saldo:.2f}")
+    print("=============================")
+
+def criar_cliente(clientes):
+    cpf = input("Informe o CPF:\t")
+    cliente = filtrar_cliente(cpf, clientes)
+    
+    if cliente:
+        print("\n@@@ Esse CPF ja existe. @@@")
+        return
+    
+    nome = input("Informe o nome:\t")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa):\t")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado):\t")
+    cliente = PessoaFisica(nome=nome,cpf=cpf,data_nascimento=data_nascimento,endereco=endereco)
+    
+    clientes.append(cliente)
+    
+    print(f"\n=== Cliente {cliente.nome} criado com sucesso. ===")
+    
+def criar_conta(numero_conta, clientes, contas):
+    cpf = input("Informe o CPF:\t")
+    cliente = filtrar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print("\n@@@ Cliente não localizado. @@@")
+        return
+    
+    conta = ContaCorrente.nova_conta(cliente=cliente, numero=numero_conta)
+    contas.append(conta)
+    cliente.contas.append(conta)
+    
+    print(f"\n=== Conta {conta.numero} criada com sucesso. ===")
+    
+def listar_contas(contas):
+    for conta in contas:
+        print("=" * 100)
+        print(textwrap.dedent(str(conta)))
+        
 def main():
     clientes = []
     contas = []
@@ -242,3 +318,8 @@ def main():
         elif opcao == "q":
             break
         
+        else:
+            print("Opção inválida, por favor tente novamente.")
+
+if __name__ == "__main__":
+    main()
